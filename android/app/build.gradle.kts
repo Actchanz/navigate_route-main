8e29fc06fc5730c0ca16d41768a507818eb1e6a3
Actchanz <kidsadaso66@nu.ac.th>
2025-09-20 19:56:20 +0700

    Homescreen

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.navigate_route"
+    namespace = "com.example.flutter_application_5"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.navigate_route"
+        applicationId = "com.example.flutter_application_5"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
